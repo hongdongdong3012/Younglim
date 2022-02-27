f6c9d51385ed4a796be1e0fbdb9b6acd0c2d9216
JuhyeYang <yjh436@gmail.com>
2022-02-27 11:45:20 +0900

    test

---
+++ js/script.js
@@ -0,0 +1,62 @@
+//메인 배너 슬라이드
+var swiper = new Swiper(".main-banner", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+
+});
+
+//sec01 슬라이드
+var swiper = new Swiper(".contents", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    // 이미지 사이 간격 조절
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var appendNumber = 4;
+var prependNumber = 1;
+document
+    .querySelector(".prepend-2-slides")
+    .addEventListener("click", function (e) {
+        e.preventDefault();
+        swiper.prependSlide([
+            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+        ]);
+    });
+document
+    .querySelector(".prepend-slide")
+    .addEventListener("click", function (e) {
+        e.preventDefault();
+        swiper.prependSlide(
+            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
+        );
+    });
+document
+    .querySelector(".append-slide")
+    .addEventListener("click", function (e) {
+        e.preventDefault();
+        swiper.appendSlide(
+            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
+        );
+    });
+document
+    .querySelector(".append-2-slides")
+    .addEventListener("click", function (e) {
+        e.preventDefault();
+        swiper.appendSlide([
+            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+        ]);
+    });
