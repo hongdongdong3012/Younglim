488af01346e5100d5fb6f3a03e7357513f7bc7ed
JuhyeYang <yjh436@gmail.com>
2022-03-06 12:48:31 +0900

    반응형수업내용

--- js/script.js
+++ js/script.js
@@ -12,52 +12,36 @@ var swiper = new Swiper(".main-banner", {
 //sec01 슬라이드
 var swiper = new Swiper(".contents", {
     slidesPerView: 3,
-    spaceBetween: 32,
+    spaceBetween: 20,
     // 이미지 사이 간격 조절
     pagination: {
         el: ".swiper-pagination",
-        type: "fraction",
+        type: "progressbar",
+
     },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+        768: {
+            slidesPerView: 3,  //브라우저가 768보다 클 때
+            spaceBetween: 32,
+        },
+    },
+
+
+});
+//sec01 슬라이드
+var swiper = new Swiper(".sec06 .swiper", {
+    slidesPerView: 2.5,
+    spaceBetween: 32,
+    breakpoints: {
+        768: {
+            slidesPerView: 3,  //브라우저가 768보다 클 때
+            spaceBetween: 40,
+        },
+    },
 });
 
-var appendNumber = 4;
-var prependNumber = 1;
-document
-    .querySelector(".prepend-2-slides")
-    .addEventListener("click", function (e) {
-        e.preventDefault();
-        swiper.prependSlide([
-            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-        ]);
-    });
-document
-    .querySelector(".prepend-slide")
-    .addEventListener("click", function (e) {
-        e.preventDefault();
-        swiper.prependSlide(
-            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
-        );
-    });
-document
-    .querySelector(".append-slide")
-    .addEventListener("click", function (e) {
-        e.preventDefault();
-        swiper.appendSlide(
-            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
-        );
-    });
-document
-    .querySelector(".append-2-slides")
-    .addEventListener("click", function (e) {
-        e.preventDefault();
-        swiper.appendSlide([
-            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
-            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
-        ]);
-    });
 
