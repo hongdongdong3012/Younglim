afb9a0f3c829813e7e4092c40d1fd93aee12584e
JuhyeYang <yjh436@gmail.com>
2022-03-13 00:23:53 +0900

    반응형팝업추가

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+//헤더 드롭 슬라이드
+var swiper = new Swiper("header .drop-area .product-wrap .swiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+});
+
 //메인 배너 슬라이드
 var swiper = new Swiper(".main-banner", {
     spaceBetween: 30,
@@ -14,7 +29,7 @@ var swiper = new Swiper(".contents", {
     spaceBetween: 20,
     // 이미지 사이 간격 조절
     pagination: {
-        el: ".swiper-pagination",
+        el: ".sec01 .swiper-pagination",
         type: "progressbar",
     },
     navigation: {
@@ -26,6 +41,7 @@ var swiper = new Swiper(".contents", {
             slidesPerView: 3,  //브라우저가 768보다 클 때
             spaceBetween: 32,
         },
+
     },
 });
 
@@ -35,7 +51,7 @@ var swiper = new Swiper(".sec05 .swiper", {
     slidesPerView: 2.5,
     spaceBetween: 10,
     pagination: {
-        el: ".swiper-pagination",
+        el: ".sec05 .swiper-pagination",
         type: "fraction",
     },
     navigation: {
